fc1f56cbe1baeacc8914ea45483b5c10a82735c4
ygold <ygold@GOLDINLAPTOP.T-mobile.com>
2018-03-23 10:57:02 -0700

    border of main grid made

--- src/SudokuFrame.java
+++ src/SudokuFrame.java
@@ -9,6 +9,7 @@ import java.awt.*;
 public class SudokuFrame extends JFrame {
 	private SudokuModel sudokuModel;
 	private MainGridButton[][] mainGridButtons;
+	private Color[] GAME_COLORS = {Color.BLUE};
 	
 	public SudokuFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,12 +32,32 @@ public class SudokuFrame extends JFrame {
 	private class MainGridButton extends JButton {
 		private static final int FONT_SIZE = 40;
 		private static final String FONT_NAME = "Arial";
+		private static final int SQUARE_BOUNDARY_BORDER_WIDTH = 4;
+		private static final int NORMAL_BORDER_WIDTH = 1;
 		
 		public MainGridButton(int row, int col) {
 			int value = sudokuModel.initialSetup().get(row).get(col);
 			if(value != 0) {
 				setText("" + value);
-				setEnabled(false);
+				setRolloverEnabled(false);
+			} else {
+				setForeground(GAME_COLORS[0]);
+			}
+			if(row == SudokuModel.SQUARES - 1 || row == SudokuModel.SQUARES*2 - 1) {
+				if(col == SudokuModel.SQUARES - 1 || col == SudokuModel.SQUARES*2 - 1) {
+					setBorder(BorderFactory.createMatteBorder(NORMAL_BORDER_WIDTH, NORMAL_BORDER_WIDTH,
+							SQUARE_BOUNDARY_BORDER_WIDTH, SQUARE_BOUNDARY_BORDER_WIDTH, Color.BLACK));
+				} else {
+					setBorder(BorderFactory.createMatteBorder(NORMAL_BORDER_WIDTH, NORMAL_BORDER_WIDTH,
+							SQUARE_BOUNDARY_BORDER_WIDTH, NORMAL_BORDER_WIDTH, Color.BLACK));
+				}
+				
+			} else if(col == SudokuModel.SQUARES - 1 || col == SudokuModel.SQUARES*2 - 1) {
+				setBorder(BorderFactory.createMatteBorder(NORMAL_BORDER_WIDTH, NORMAL_BORDER_WIDTH,
+						NORMAL_BORDER_WIDTH, SQUARE_BOUNDARY_BORDER_WIDTH, Color.BLACK));
+			} else {
+				setBorder(BorderFactory.createMatteBorder(NORMAL_BORDER_WIDTH, NORMAL_BORDER_WIDTH,
+						NORMAL_BORDER_WIDTH, NORMAL_BORDER_WIDTH, Color.BLACK));
 			}
 			setFont(new Font(FONT_NAME, Font.PLAIN, FONT_SIZE));
 		}
